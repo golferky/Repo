db3e43429fe3c1c1eaa8a7c201013ee837dba543
Gary Scudder <gary_scudder@yahoo.com>
2026-05-22 09:18:39 -0400

    Squashed 'epgmanager/' content from commit 3f5e0da
    git-subtree-dir: epgmanager
    git-subtree-split: 3f5e0dafeff236b4c1b274aea589ca4e6b39b3b5

---
+++ ChannelLookup.vb
@@ -0,0 +1,109 @@
+﻿Imports Microsoft.Data.Sqlite
+
+Public Module ChannelLookup
+
+    Public Function GetStreamId(dbPath As String,
+                                channelId As String) As String
+
+        Using con As New SqliteConnection($"Data Source={dbPath}")
+            con.Open()
+
+            Dim cmd As New SQLiteCommand("
+                SELECT stream_id
+                FROM channels
+                WHERE lower(channel_id)=@c
+                LIMIT 1", con)
+
+            cmd.Parameters.AddWithValue("@c", channelId.ToLower())
+
+            Dim result = cmd.ExecuteScalar()
+
+            If result Is Nothing Then
+                Return Nothing
+            End If
+
+            Return result.ToString()
+
+        End Using
+
+    End Function
+    Public Function IsForeign(db As String, channel As String) As Boolean
+
+        Using conn As New SQLiteConnection("Data Source=" & db)
+            conn.Open()
+
+            Dim cmd As New SQLiteCommand(
+                "SELECT is_foreign FROM channels WHERE channel_id=@c", conn)
+
+            cmd.Parameters.AddWithValue("@c", channel)
+
+            Dim result = cmd.ExecuteScalar()
+
+            If result Is Nothing Then Return False
+
+            Return Convert.ToInt32(result) = 1
+
+        End Using
+
+    End Function
+    Public Function IsMovieChannel(db As String, channel As String) As Boolean
+
+        Using conn As New SQLiteConnection("Data Source=" & db)
+            conn.Open()
+
+            Dim cmd As New SQLiteCommand(
+                "SELECT is_movie_channel FROM channels WHERE channel_id=@c", conn)
+
+            cmd.Parameters.AddWithValue("@c", channel)
+
+            Dim result = cmd.ExecuteScalar()
+
+            If result Is Nothing Then Return False
+
+            Return Convert.ToInt32(result) = 1
+
+        End Using
+
+    End Function
+    Public Function GetMyChannel(db As String, channel As String) As String
+
+        Using conn As New SQLiteConnection("Data Source=" & db)
+            conn.Open()
+
+            Dim cmd As New SQLiteCommand(
+                "SELECT my_channel FROM channels WHERE channel_id=@c", conn)
+
+            cmd.Parameters.AddWithValue("@c", channel)
+
+            Dim result = cmd.ExecuteScalar()
+
+            If result Is Nothing Then Return ""
+
+            Return result.ToString()
+
+        End Using
+
+    End Function
+    Public Function GetChannelInfo(db As String, channel As String) As (String, String)
+
+        Using conn As New SQLiteConnection("Data Source=" & db)
+            conn.Open()
+
+            Dim cmd As New SQLiteCommand(
+                "SELECT nickname, my_channel FROM channels WHERE channel_id=@c", conn)
+
+            cmd.Parameters.AddWithValue("@c", channel)
+
+            Using rdr = cmd.ExecuteReader()
+                If rdr.Read() Then
+                    Return (rdr("nickname").ToString(), rdr("my_channel").ToString())
+                End If
+            End Using
+
+        End Using
+
+        Return ("", "")
+
+    End Function
+
+End Module
---
+++ push_epg.ps1
@@ -0,0 +1,5 @@
+cd C:\Users\Gary\source\repos\epgmanager
+
+git add .
+git commit -m "fix empty folders"
+git push
